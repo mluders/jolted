5eda4b38cce46597c016cae1dc787963357aefce
Miles Luders <mpluders@gmail.com>
2020-05-29 14:04:45 -0700

    Basic spinning wheel

--- app/controllers/proxy/snippets_controller.rb
+++ app/controllers/proxy/snippets_controller.rb
@@ -1,6 +1,6 @@
 module Proxy
   class SnippetsController < ApplicationController
-   skip_before_action :verify_authenticity_token, only: [:show]
+    skip_before_action :verify_authenticity_token, only: [:show]
 
     def show; end
   end
--- app/controllers/proxy/wheels_controller.rb
+++ app/controllers/proxy/wheels_controller.rb
@@ -1,6 +1,8 @@
 module Proxy
   class WheelsController < ApplicationController
-    def show
+    def create
+      email = params.require(:email)
+      render json: { coupon_code: 'abc123' }, status: :created
     end
   end
 end
