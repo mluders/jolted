8e2e9b4420e96583c5f2c5f1c785e39f7155a530
Miles Luders <mpluders@gmail.com>
2020-06-17 09:42:49 -0700

    Create basic wheel animation using react-spring

--- app/javascript/apps/joltify/packs/src/shared/Wheel.jsx
+++ app/javascript/apps/joltify/packs/src/shared/Wheel.jsx
@@ -1,5 +1,26 @@
 import React from 'react';
+import { useSpring, animated } from 'react-spring'
+import { easeCubicOut } from 'd3-ease';
+
+import wheelImage from './wheel_image.png';
 
 export default function Wheel() {
-  return <h1>Hello</h1>
+  const imageStyle = useSpring({
+    transform: 'rotate(0deg)',
+    from: {
+      transform: 'rotate(-1800deg)',
+      maxHeight: '250px',
+      position: 'absolute'
+    },
+    config: {
+      duration: 5000,
+      easing: easeCubicOut
+    }
+  });
+
+  return (
+    <div>
+      <animated.img style={imageStyle} src={wheelImage}></animated.img>
+    </div>
+  )
 }
