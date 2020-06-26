be4d6a7238068e3068cd5e8a179655ca385b2b6f
Miles Luders <mpluders@gmail.com>
2020-06-26 09:43:19 -0800

    Add validations to DiscountCode model

--- app/models/discount_code.rb
+++ app/models/discount_code.rb
@@ -1,6 +1,12 @@
 class DiscountCode < ApplicationRecord
   belongs_to :wheel
 
+  validates :email, presence: true
+  validates :code, presence: true
+  validates :expires_at, presence: true
+  validates :shopify_price_rule_id, presence: true
+  validates :shopify_discount_code_id, presence: true
+
   def expired?
     self.expires_at < DateTime.now.utc
   end
