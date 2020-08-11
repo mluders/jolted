4ca9ae996a48fb62b457fda258d6d32c25b0d599
Miles Luders <mpluders@gmail.com>
2020-08-11 09:25:38 -0700

    Add dummy line to trigger reinstall

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,6 +2,8 @@
 
 class HomeController < AuthenticatedController
   def index
+    ShopifyAPI::Shop.current # Dummy code to trigger a re-install if necessary
+
     @wheel = Wheel.find_by(shop: current_shop)
   end
 end
